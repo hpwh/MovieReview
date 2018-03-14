95d25c9523cdf7794d6d8a7b951da461be1294d3
Hanna Pawahi <hpawahi@gmail.com>
2018-03-14 10:00:40 +0100

    rensat upp

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,11 @@ module.exports = {
     mode: "development",
     entry: './code.js', 
     output: {
-      path: path.resolve(__dirname, "."), 
+      path: path.resolve(__dirname, "distribution"), 
       filename: 'bundle.js',
     },
     devServer: {
-      contentBase: path.join(__dirname, "."), // the root for the server
+      contentBase: path.join(__dirname, "distribution"), // the root for the server
       watchContentBase: true, // so we reload if other stuff like CSS changes
       port: 9000, // it'll now be at http://localhost:9000
     },
